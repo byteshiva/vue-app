e96f7619b1ada43782775a9f0d310ee539029032
Siva <byteshiva@users.noreply.github.com>
2021-10-04 07:29:28 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-app/'
+    ? '/'
     : '/',
   configureWebpack: {
     devServer: {
